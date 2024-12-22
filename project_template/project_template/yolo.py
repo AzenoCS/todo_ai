f27de988500a278c73688ee9484efcefd92e44f8
cezary <cezary.szukiel@gmail.com>
2024-12-22 11:45:05 +0100

    ruff formatter config

---
+++ project_template/project_template/yolo.py
@@ -0,0 +1,14 @@
+import os
+
+
+def add(a: int, b: int) -> int:
+    """
+    Add two numbers together
+    :param a: virtualy
+    :param b: dumies
+    :return:
+    """
+    return a + b
+
+
+x = 42
